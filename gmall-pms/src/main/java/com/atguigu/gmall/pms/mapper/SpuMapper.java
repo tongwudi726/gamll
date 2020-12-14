671bcaf2058857e0106a8a6fdb085573ce236206
wc-tc <2994844299@qq.com>
2020-12-14 20:18:44 +0800

    谷粒商城项目搭建

---
+++ gmall-pms/src/main/java/com/atguigu/gmall/pms/mapper/SpuMapper.java
@@ -0,0 +1,17 @@
+package com.atguigu.gmall.pms.mapper;
+
+import com.atguigu.gmall.pms.entity.SpuEntity;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import org.apache.ibatis.annotations.Mapper;
+
+/**
+ * spu信息
+ * 
+ * @author tongwudi
+ * @email tongwudi@atguigu.com
+ * @date 2020-12-14 20:04:19
+ */
+@Mapper
+public interface SpuMapper extends BaseMapper<SpuEntity> {
+	
+}
